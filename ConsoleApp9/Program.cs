183b8b13776f27e6558a5c2ac7841ee2d2a8c278
Дмитрий Стародуб <sergeistarodyb@gmail.com>
2024-11-05 00:30:27 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp9/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Задача 1.Определить, имеются ли в одномерном массиве случайных чисел элементы, значения которых кратны 11.Вывести такие значения.
+            //int[] omas = new int[] { 22, 35, 11, 50, 99, 77, 88, 33, 121 };
+            //int count = 0;
+            //Console.WriteLine("Элементы массива, кратные 11:");
+            //for (int i = 0; i < omas.Length; i++)
+            //{
+            //    if (omas[i] % 11 == 0)
+            //    {
+            //        Console.WriteLine(omas[i]);
+            //        count++;
+            //    }
+            //}
+
+            //if (count == 0)
+            //{
+            //    Console.WriteLine("Нет элементов, кратных 11.");
+            //}
+            //Console.ReadKey();
+//        }
+//    }
+//}
+
+            //Задача 2.Найти сумму и количество положительных элементов в одномерном массиве с явной инициализацией.
+            //int[] omas = new int[] { 5, -4, 8, 7, -2 };
+            //int value = 0;
+            //int count = 0;
+            //for (int i = 0; i < omas.Length; i++)
+            //{
+            //    if (omas[i] > 0)
+            //    {
+            //        value += omas[i];
+            //        count++;
+
+            //    }
+            //}
+            //Console.WriteLine($"Сумма: {value}\n колво:{count}");
+            //Console.ReadKey();
+
+            //Задача 3. Вывести элементы одномерного массива, заполненного с клавиатуры ,в обратном порядке.
+            //const int n = 6;
+            //int[] mass = new int[n];
+            //Random rng = new Random();
+            //for(int i = 0; i<mass.Length; i++)
+            //{
+            //    mass[i] = rng.Next(0, 99);
+            //    Console.WriteLine($"omas[{i}] = {mass[i]}");
+            //}
+            //for (int i = mass.Length - 1; i > mass.Length; i--)
+            //{
+            //    Console.WriteLine(mass[i]);
+            //}
+            //Console.ReadKey();
+        }
+    }
+}
